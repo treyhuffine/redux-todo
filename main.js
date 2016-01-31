6a7724e9997c0f680e68eeabeee62b9aded799e6
Trey Huffine <treyhuffine@gmail.com>
2016-01-31 12:04:38 -0800

    Add visibility filter and make code more scalable

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from './create-store';
-import { todos } from './reducers/todos';
+import { createStore } from './redux_functions/createStore';
+// import { todos } from './reducers/todos';
+import { todoApp } from './reducers/todoApp';
 
-
-const store = createStore(todos);
+const store = createStore(todoApp);
 console.log(store);
 const render = () => {
   ReactDOM.render (
@@ -27,6 +27,11 @@ console.log(store.getState());
 store.dispatch({
   type: 'TOGGLE_TODO',
   id: 1
+});
+console.log(store.getState());
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
 });
 console.log(store.getState());
 
---
+++ reducers/todoApp.js
@@ -0,0 +1,15 @@
+import { todos } from './todos';
+import { visibilityFilter } from './visibilityFilter';
+
+export const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
+  }
+}
